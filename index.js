d78bf6ffcb189599e05853d86b56d124e56a1c24
venkat ravilla <venkatravilla333@gmail.com>
2024-11-12 18:18:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+
+
+// primitives
+
+// console.log(a)
+// var a = 'sachin'
+// console.log(a, typeof a)
+
+// console.log(b)
+// var b = 40
+// console.log(b, typeof b)
+
+// console.log(c)
+// var c = false
+// console.log(c, typeof c)
+
+// console.log(d)
+// var d
+// console.log(d, typeof d)
+
+// console.log(e)
+// var e = null
+// console.log(e, typeof e)
+
+//reference
+
+
+// var person = {
+//   name: 'sachin',
+//   age: 40,
+//   address: {city: 'mumbai', road: 201}
+// }
+
+// console.log(person)
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.address.city)
+// console.log(person.address.road)
+// console.log(person[age])
+
+
+// var names = ['sachin', 'dhoni', 'kohli']
+
+// console.log(names)
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+
+// var x = 10
+
+// var test = ()=> {
+//   var x = 100
+// }
+
+// test()
