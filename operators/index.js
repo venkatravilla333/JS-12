9f4fc6983cef89f1d82784f5465db12a6299060b
venkat ravilla <venkatravilla333@gmail.com>
2024-11-15 18:05:33 +0530

    upto operators

---
+++ operators/index.js
@@ -0,0 +1,133 @@
+
+
+// arithmatic
+
+
+// var a = 10
+
+// var b = 20
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a ** b)
+
+// console.log(a)
+// console.log(a++)
+// console.log(a)
+// console.log(a++)
+// console.log(++a)
+// console.log(a--)
+// console.log(--a)
+// console.log(a--)
+// console.log(++a)
+
+//assignment
+
+var a = 10
+
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+
+// a += 100
+// console.log(a)
+// a -= 100
+// console.log(a)
+// a *= 100
+// console.log(a)
+// a /= 100
+// console.log(a)
+// a %= 100
+// console.log(a)
+// a **= 100
+// console.log(a)
+
+
+// comparision
+
+// var a = 10
+// var b = 11
+
+//relational
+
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a < b)
+// console.log(a <= b)
+
+//equality
+
+
+// var a = 10
+// var b = 11
+// var b = 10
+// var b = '10'
+// var a = null
+// var b = undefined
+// var a = {}
+// var b = {}
+// var a = {value: 10}
+// var b = {value: 10}
+// var a = {value: 10}
+// var b = a
+// var a = []
+// var b = []
+// var a = [1,2,3]
+// var b = []
+// var a = [1,2,3]
+// var b = a
+// var a = ()=>{}
+// var b = ()=>{}
+// var a = ()=>{}
+// var b = a
+
+// var a = 10
+// var b = { value: 10 }
+
+// console.log(a == b)
+// console.log(a === b)
+
+// inequality
+
+// var a = 10
+// var b = 10
+
+// console.log(a == b) //loose equality
+// console.log(a === b) //strict equality
+// console.log(a != b) //loose inequality
+// console.log(a !== b) //strict inequality
+
+//ternary operator
+
+// var age = 20
+
+// var result = age >= 18 ? 'having vote' : 'not having vote'
+
+// if (age >= 18) {
+//   console.log('having vote')
+// } else {
+//   console.log('not having vote')
+// }
+
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
