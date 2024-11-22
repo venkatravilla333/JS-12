3ceddd8ed12013148268bce72641b078f1b8f649
venkat ravilla <venkatravilla333@gmail.com>
2024-11-22 18:00:06 +0530

    loops completed

---
+++ loops/index.js
@@ -0,0 +1,212 @@
+
+
+// var a = 'sachin'
+
+//without loop
+
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+// console.log(a)
+
+// conditional loops
+
+// for loop
+
+// for (var i = 1; i <= 10; i++){
+//   console.log(i, a)
+// }
+
+//while loop
+
+// var i = 1
+
+// while (i<=10) {
+//   console.log(i, a)
+//   i++
+// }
+
+
+//do while loop
+
+// var i = 11
+
+// while (i<=10) {
+//   console.log(i, a)
+//   i++
+// }
+
+// do {
+//   console.log(i, a)
+//   i++
+// } while (i <= 10)
+
+//infinity loop
+
+// for (var i = 0; i >= 0; i++){
+//   console.log(i, a)
+// }
+  
+
+//break and continue
+
+// for (var i = 1; i <= 10; i++){
+//   if(i===5) continue
+//   console.log(i)
+// }
+
+
+var obj = {
+  name: 'sachin',
+  age: 40,
+  city:'mumbai', 
+  color: 'red'
+}
+// console.log(obj)
+
+// without loop
+
+// console.log(obj.name)
+// console.log(obj.age)
+// console.log(obj.city)
+// console.log(obj.color)
+// console.log(obj['name'])
+// console.log(obj['age'])
+// console.log(obj['city'])
+// console.log(obj['color'])
+
+
+// with using loop
+
+// for (var x in obj) {
+//     console.log(x, obj[x])
+// }
+
+// for (var x of obj) {
+//     console.log(x, obj[x])
+// }
+
+var iterableds = [1, 2, 3, 4, 5]
+
+// var iteratorObj = iterableds[Symbol.iterator]()
+
+// console.log(iteratorObj)
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+
+// without loop
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// for (var key in iterableds) {
+//   console.log(key, iterableds[key])
+// }
+// for (var value of iterableds) {
+//   console.log(value)
+// }
+
