fa46f6705e59a513a4eb13c62ca5b04efe19155b
venkat ravilla <venkatravilla333@gmail.com>
2024-12-04 20:11:51 +0530

    object completed

--- Objects/index.js
+++ Objects/index.js
@@ -214,3 +214,161 @@ var obj = {
 // console.log(Math.sqrt(25))
 
 
+//Date object
+
+// let now = new Date()
+// console.log(now)
+
+//get methods
+
+// console.log(now.getFullYear())
+// console.log(now.getMonth())
+// console.log(now.getDate())
+// console.log(now.getDay())
+// console.log(now.getHours())
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+// console.log(now.getMilliseconds())
+
+//set methods
+
+// console.log(now.setFullYear(2050))
+// console.log(now.setMonth(10))
+// console.log(now.setDate(2))
+// console.log(now.setMinutes(30))
+// console.log(now.setHours(10))
+// console.log(now.setSeconds(30))
+// console.log(now)
+
+//number (custom date)
+
+// let date1 = new Date(2047, 7, 10, 7, 30, 45)
+// console.log(date1)
+// console.log(date1.getFullYear())
+// date1.setFullYear(1947)
+// console.log(date1)
+
+//string (custom date)
+
+// let date2 = new Date('2047 feb 10 9:40:30')
+// console.log(date2)
+// console.log(date2.getFullYear())
+// date2.setFullYear(1947)
+// console.log(date2)
+
+
+//ES-5 syntax
+
+// var name = 'sachin'
+// var age = 40
+
+// console.log('Hi this is' + ' ' + name + ' ' + 'my age is' + ' ' + age)
+
+//ES-6 Template string
+
+// console.log(`Hi this is ${name} my age is ${age}`)
+
+
+//String object
+
+// let msg = 'Hi how are you'
+
+//how to find string length
+// console.log(msg.length)
+
+// how to trim spaces
+
+// let msg = ' Hi how are you '
+// console.log(msg.length)
+// console.log(msg.trimStart().length)
+// console.log(msg.trimEnd().length)
+// console.log(msg.trim().length)
+
+//how to extract a part of string
+// let msg = 'Hi how are you'
+
+// let part = msg.slice(3, 6)
+// let part = msg.slice(3, 6)
+// let part = msg.slice()
+// console.log(part)
+
+//how to extract a string character
+
+// let msg = 'Hi how are you'
+
+// var char = msg.charAt(0)
+// console.log(char)
+
+// var charcode = msg.charCodeAt(0)
+// console.log(charcode)
+
+//how to convert case
+// let msg = 'Hi how are you'
+// let msg = 'sachin'
+
+// console.log(msg.toLowerCase())
+// console.log(msg.toUpperCase())
+// console.log(msg.charAt(0).toUpperCase())
+
+
+//how to add new string at the end of string
+
+// let msg = 'Hi how are you'
+
+// let newStr = msg.concat(` sachin`)
+// console.log(newStr)
+
+//how to replace existing string with new string
+// let msg = 'Hi how are you'
+// console.log(msg.replace('You', 'me'))
+
+//how to convert string in array
+
+// let msg = 'Hi how are you'
+
+// let arr = msg.split()
+// let arr = msg.split('')
+// let arr = msg.split(' ')
+// console.log(arr)
+
+//how to add or  pad new string
+
+// let msg = 'hi'
+
+// console.log(msg.padStart(7, 10))
+// console.log(msg.padEnd(7, 10))
+
+//how to find specific string
+
+// let msg = 'Hi how are Hi you Hi'
+
+// console.log(msg.startsWith('Hi'))
+// console.log(msg.startsWith('bye'))
+// console.log(msg.endsWith('you'))
+// console.log(msg.endsWith('bye'))
+// console.log(msg.includes('you'))
+// console.log(msg.endsWith('bye'))
+// console.log(msg.indexOf('Hi'))
+// console.log(msg.indexOf('bye'))
+// console.log(msg.lastIndexOf('Hi'))
+// console.log(msg.lastIndexOf('bye'))
+
+//how to repeat string
+
+// var msg = 'hi how are you'
+
+// console.log(msg.repeat(4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
