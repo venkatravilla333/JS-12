f2aa93b30cb311b1b755fb3245cd9c7cbd0ea201
venkat ravilla <venkatravilla333@gmail.com>
2024-12-28 19:04:53 +0530

    type conversion

---
+++ 18.Type-conversion/index.js
@@ -0,0 +1,104 @@
+
+
+// console.log("4" + 'sachin')
+// console.log("4" + 4)
+// console.log("4" + true)
+// console.log("4" + false)
+// console.log("4" + undefined)
+// console.log("4" + null)
+// console.log("4" + {})
+// console.log("4" + [])
+// console.log("4" + function test(){})
+
+
+// console.log("4" - "sachin")
+// console.log("4" - 2)
+// console.log("4" - true)
+// console.log("4" - false)
+// console.log("4" - undefined)
+// console.log("4" - null)
+// console.log("4" - {})
+// console.log("4" - [])
+// console.log("4" - function test(){})
+
+// console.log(4 - "sachin")
+// console.log(4 - 2)
+// console.log(4 - false)
+// console.log(4 - true)
+// console.log(4 - undefined)
+// console.log(4 - null)
+// console.log(4 - {})
+// console.log(4 - [])
+// console.log(4 - function test(){})
+
+// console.log(undefined + 4)
+// console.log(undefined + undefined)
+// console.log(undefined + "hi")
+// console.log(undefined + null)
+// console.log(undefined + true)
+// console.log(undefined + false)
+
+// console.log(undefined + {})
+// console.log(undefined + [])
+// console.log(undefined + function test(){})
+
+// console.log(null + "sachin")
+// console.log(null + 4)
+// console.log(null + true)
+// console.log(null + false)
+// console.log(null + undefined)
+// console.log(null + null)
+// console.log(null + {})
+// console.log(null + [])
+// console.log(null + function test(){})
+
+
+// console.log(null - "sachin")
+// console.log(null - 4)
+// console.log(null - true)
+// console.log(null - false)
+// console.log(null - undefined)
+// console.log(null - null)
+// console.log(null - {})
+// console.log(null - [])
+// console.log(null - function test(){})
+
+// console.log(true + 'sachin')
+// console.log(true + 4)
+// console.log(true + true)
+// console.log(true + false)
+// console.log(true + undefined)
+// console.log(true + null)
+// console.log(true + {})
+// console.log(true + [])
+// console.log(true + function test(){})
+
+
+// console.log(true - 'sachin')
+// console.log(true - 4)
+// console.log(true - true)
+// console.log(true - false)
+// console.log(true - undefined)
+// console.log(true - null)
+// console.log(true - {})
+// console.log(true - [])
+// console.log(true - function test(){})
+
+
+
+// explicit conversion
+
+// let x = 4
+
+// console.log(x, typeof x)
+
+// let y = String(x)
+// console.log(y, typeof y)
+
+// let z = Boolean(y)
+
+// console.log(z, typeof z)
+
+// let a = Number(z)
+
+// console.log(a, typeof a)
